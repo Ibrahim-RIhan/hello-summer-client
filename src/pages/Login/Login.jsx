877d0bd6c43bab3c3094b98d63365522e7bffbaa
rihan010 <ibrhimrihan000@gmail.comm>
2023-06-07 23:50:34 +0600

    registration page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,82 +1,107 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import {FcGoogle } from 'react-icons/fc';
+import { FcGoogle } from "react-icons/fc";
+import {BsFillEyeFill, BsFillEyeSlashFill} from "react-icons/bs"
+import image from "../../assets/images/HelloSummerLogo.png";
 
 const Login = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [password, setPassword] = useState("");
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
     return (
         <section className="bg-white">
-        <div className="grid grid-cols-1 lg:grid-cols-2">
-            <div className="flex items-center justify-center px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
-                <div className="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
-                    <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Login to Hello Summer !</h2>
-                    <p className="mt-2 text-base text-gray-600">Already have an account? <Link to="/register" className="font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 hover:underline focus:text-blue-700">Please register !</Link></p>
-    
-                    <form className="mt-8">
-                        <div className="space-y-5">
-                            <div>
-                                <label className="text-base font-medium text-gray-900">Email address </label>
-                                <div className="mt-2.5">
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div className="flex items-center justify-center px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
+                    <div className="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
+                        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
+                            Login to Hello Summer!
+                        </h2>
+                        <p className="mt-2 text-base text-gray-600">
+                            Already have an account?{" "}
+                            <Link
+                                to="/register"
+                                className="font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 hover:underline focus:text-blue-700"
+                            >
+                                Please register!
+                            </Link>
+                        </p>
+
+                        <form className="mt-8">
+                            <div className="space-y-5">
+                                <div>
+                                    <label className="text-base font-medium text-gray-900">
+                                        Email address
+                                    </label>
+                                    <div className="mt-2.5">
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            placeholder="Enter email to get started"
+                                            className="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label className="text-base font-medium text-gray-900">
+                                        Password
+                                    </label>
+                                    <div className="mt-2.5 relative">
+                                        <input
+                                            type={passwordVisible ? "text" : "password"}
+                                            name="password"
+                                            placeholder="Enter your password"
+                                            value={password}
+                                            onChange={handlePasswordChange}
+                                            className="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                                        />
+                                        <span
+                                            className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                                            onClick={togglePasswordVisibility}
+                                        >
+                                            {passwordVisible ?
+                                                <BsFillEyeSlashFill></BsFillEyeSlashFill>
+                                         : <BsFillEyeFill></BsFillEyeFill>
+                                            }
+                                        </span>
+                                    </div>
+                                </div>
+                                <div>
                                     <input
-                                        type="email"
-                                        name="email"
-                                        placeholder="Enter email to get started"
-                                        className="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
+                                        type="submit"
+                                        value="Login"
+                                        className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700"
                                     />
                                 </div>
                             </div>
-    
-                            <div>
-                                <label  className="text-base font-medium text-gray-900"> Password </label>
-                                <div className="mt-2.5">
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        placeholder="Enter your password"
-                                        className="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
-                                    />
-                                </div>
-                            </div>
-                            <div>
-                                <button type="submit" className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700">
-                                    Login
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-    
-                    <div className="mt-3 space-y-3">
-                        <button
-                            type="button"
-                            className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
-                        >
-                           <FcGoogle></FcGoogle>  Sign up with Google
-                        </button>
-    
-                      
-                    </div>
-                </div>
-            </div>
-    
-            <div className="flex items-center justify-center px-4 py-10 sm:py-16 lg:py-24 bg-gray-50 sm:px-6 lg:px-8">
-                <div>
-                    <img className="w-full mx-auto" src="https://cdn.rareblocks.xyz/collection/celebration/images/signup/1/cards.png" alt="" />
-    
-                    <div className="w-full max-w-md mx-auto xl:max-w-xl">
-                        <h3 className="text-2xl font-bold text-center text-black">Design your own card</h3>
-                        <p className="leading-relaxed text-center text-gray-500 mt-2.5">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
-    
-                        <div className="flex items-center justify-center mt-10 space-x-3">
-                            <div className="bg-orange-500 rounded-full w-20 h-1.5"></div>
-    
-                            <div className="bg-gray-200 rounded-full w-12 h-1.5"></div>
-    
-                            <div className="bg-gray-200 rounded-full w-12 h-1.5"></div>
+                        </form>
+
+                        <div className="mt-3 space-y-3">
+                            <button
+                                type="button"
+                                className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
+                            >
+                                <FcGoogle></FcGoogle> Sign up with Google
+                            </button>
                         </div>
                     </div>
                 </div>
+
+                <div className="flex items-center justify-center px-4 py-10 sm:py-16 lg:py-24 sm:px-6 lg:px-8">
+                    <div>
+                        <img className="w-full mx-auto" src={image} alt="" />
+                    </div>
+                </div>
             </div>
-        </div>
-    </section>
-    
+        </section>
     );
 };
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import image from "../../assets/images/HelloSummerLogo.png";
 
 const Register = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="bg-white ">
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className="text-center lg:text-left">
+                        <div>
+                            <img className="w-full mx-auto" src={image} alt="" />
+                        </div>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" required name="name" placeholder="Your Name" className="input input-bordered input-info w-full max-w-xs" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" required name="email" placeholder="Your Email" className="input input-bordered input-info w-full max-w-xs" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="Password" required placeholder="Your Password" className="input input-bordered input-info w-full max-w-xs" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Confirm Password</span>
+                                </label>
+                                <input type="password" required placeholder="Confirm Your Password" className="input input-bordered input-info w-full max-w-xs" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Choose Your Photo</span>
+                                </label>
+                                <input type="file" className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+                            </div>
+                            <div className="form-control mt-6">
+                               <input className="btn" type="submit" value="Register" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
