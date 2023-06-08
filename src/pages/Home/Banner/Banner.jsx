7b313c562debf695023f3b909c8f078fb7653884
rihan010 <ibrhimrihan000@gmail.comm>
2023-06-08 15:25:34 +0600

    extra section added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -13,35 +13,72 @@ const Banner = () => {
 
     return (
         <div>
-            <Carousel className='text-center' autoPlay:true autoFocus: transitionTime="700" >
+            <Carousel className='text-center' autoPlay:true transitionTime="700" >
                 <div className='relative'>
                     <img className="h-1/4 rounded-lg" src={bannerImage1} />
                     <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
                         <div className="w-3/4 text-center md:w-2/4">
                             <Fade delay={500} duration={1500} fraction={0} cascade direction="left" >
                                 <h1 className='text-xl lg:text-5xl text-white'>Explore various kind of art and Crafts</h1>
-                                <p className='text-white'>Feel Hello Summer</p>
+                                <p className='text-white mt-3'>Feel Hello Summer</p>
                             </Fade>
                         </div>
                     </div>
                 </div>
-                <div>
+                <div className='relative'>
                     <img className="h-1/4 rounded-lg" src={bannerImage2} />
-
+                    <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+                        <div className="w-3/4 text-center md:w-2/4">
+                            <Fade delay={500} duration={1500} fraction={0} cascade direction="left" >
+                                <h1 className='text-xl lg:text-5xl text-white'>Explore various kind of art and Crafts</h1>
+                                <p className='text-white mt-3'>Feel Hello Summer</p>
+                            </Fade>
+                        </div>
+                    </div>
                 </div>
-                <div>
+                <div className='relative'>
                     <img className="h-1/4 rounded-lg" src={bannerImage3} />
+                    <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+                        <div className="w-3/4 text-center md:w-2/4">
+                            <Fade delay={500} duration={1500} fraction={0} cascade direction="left" >
+                                <h1 className='text-xl lg:text-5xl text-white'>Explore various kind of art and Crafts</h1>
+                                <p className='text-white mt-3'>Feel Hello Summer</p>
+                            </Fade>
+                        </div>
+                    </div>
                 </div>
-                <div>
+                <div className='relative'>
                     <img className="h-1/4 rounded-lg" src={bannerImage4} />
-
+                    <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+                        <div className="w-3/4 text-center md:w-2/4">
+                            <Fade delay={500} duration={1500} fraction={0} cascade direction="left" >
+                                <h1 className='text-xl lg:text-5xl text-white'>Explore various kind of art and Crafts</h1>
+                                <p className='text-white mt-3'>Feel Hello Summer</p>
+                            </Fade>
+                        </div>
+                    </div>
                 </div>
-                <div>
+                <div className='relative'>
                     <img className="h-1/4 rounded-lg" src={bannerImage5} />
-
+                    <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+                        <div className="w-3/4 text-center md:w-2/4">
+                            <Fade delay={500} duration={1500} fraction={0} cascade direction="left" >
+                                <h1 className='text-xl lg:text-5xl text-white'>Explore various kind of art and Crafts</h1>
+                                <p className='text-white mt-3'>Feel Hello Summer</p>
+                            </Fade>
+                        </div>
+                    </div>
                 </div>
-                <div>
+                <div className='relative'>
                     <img className="h-1/4 rounded-lg" src={bannerImage6} />
+                    <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+                        <div className="w-3/4 text-center md:w-2/4">
+                            <Fade delay={500} duration={1500} fraction={0} cascade direction="left" >
+                                <h1 className='text-xl lg:text-5xl text-white'>Explore various kind of art and Crafts</h1>
+                                <p className='text-white mt-3'>Feel Hello Summer</p>
+                            </Fade>
+                        </div>
+                    </div>
                 </div>
 
             </Carousel>
