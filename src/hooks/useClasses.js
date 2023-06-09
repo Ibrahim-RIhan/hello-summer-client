57543db447adf63da0253cbbc7dd838a9597a37d
rihan010 <ibrhimrihan000@gmail.comm>
2023-06-09 19:40:51 +0600

    axios Secure added

--- src/hooks/useClasses.js
+++ src/hooks/useClasses.js
@@ -1,12 +1,13 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useState } from "react";
+import { AuthContext } from "../providers/AuthProvider/AuthProvider";
 
 
 const useClasses = () => {
-    
+    const {user} = useContext(AuthContext)
    const [classes, setClasses] =useState([])
    useEffect(()=>{
-    fetch('http://localhost:5000/classes')
+    fetch(`http://localhost:5000/?email=${user?.email}`)
     .then(res=> res.json())
     .then(data =>setClasses(data))
    },[])
