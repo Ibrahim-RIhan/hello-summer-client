5c40037b85cfc757ae6f8204aa01736d1d4d6f4f
rihan010 <ibrhimrihan000@gmail.comm>
2023-06-11 21:44:51 +0600

    use selected class

--- src/hooks/useSelectedClass.js
+++ src/hooks/useSelectedClass.js
@@ -1,9 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider/AuthProvider';
 
 
 const useSelectedClass = () => {
+    const {user} =useContext(AuthContext)
     const { data: selectedClasses = [], refetch } = useQuery(['selectedClass'], async () => {
-        const res = await fetch('http://localhost:5000/selectedClass')
+        const res = await fetch(`http://localhost:5000/selectedClass/${user?.email}`)
         return res.json();
     })
     return [selectedClasses, refetch];
